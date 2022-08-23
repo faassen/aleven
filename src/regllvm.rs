48a961fffad22c626adbadecd61ec5ff54caf4fb
Martijn Faassen <faassen@startifact.com>
2022-08-23 17:16:28 +0200

    Cleanup

--- src/regllvm.rs
+++ src/regllvm.rs
@@ -7,12 +7,6 @@ use inkwell::values::{IntValue, PointerValue};
 use inkwell::{AddressSpace, OptimizationLevel};
 use std::error::Error;
 
-/// Convenience type alias for the `sum` function.
-///
-/// Calling this is innately `unsafe` because there's no guarantee it doesn't
-/// do `unsafe` operations internally.
-type SumFunc = unsafe extern "C" fn(u64, u64, u64) -> u64;
-
 type ProgramFunc = unsafe extern "C" fn(*mut u8) -> ();
 
 struct CodeGen<'ctx> {
@@ -105,26 +99,6 @@ impl<'ctx> CodeGen<'ctx> {
         let sum = self.builder.build_int_add(rs1, rs2, "add");
         registers[register.rd as usize] = sum;
     }
-
-    fn jit_compile_sum(&self) -> Option<JitFunction<SumFunc>> {
-        let i64_type = self.context.i64_type();
-        let fn_type = i64_type.fn_type(&[i64_type.into(), i64_type.into(), i64_type.into()], false);
-        let function = self.module.add_function("sum", fn_type, None);
-        let basic_block = self.context.append_basic_block(function, "entry");
-
-        self.builder.position_at_end(basic_block);
-
-        let x = function.get_nth_param(0)?.into_int_value();
-        let y = function.get_nth_param(1)?.into_int_value();
-        let z = function.get_nth_param(2)?.into_int_value();
-
-        let sum = self.builder.build_int_add(x, y, "sum");
-        let sum = self.builder.build_int_add(sum, z, "sum");
-
-        self.builder.build_return(Some(&sum));
-
-        unsafe { self.execution_engine.get_function("sum").ok() }
-    }
 }
 
 pub fn main() -> Result<(), Box<dyn Error>> {
@@ -165,35 +139,8 @@ pub fn main() -> Result<(), Box<dyn Error>> {
     }
     println!("Expecting memory");
     println!("{:?}", memory);
-    // assert_eq!(memory[2], 12);
-    // assert_eq!(memory[1], 11);
 
     Ok(())
-
-    // let context = Context::create();
-    // let module = context.create_module("sum");
-    // let execution_engine = module.create_jit_execution_engine(OptimizationLevel::None)?;
-    // let codegen = CodeGen {
-    //     context: &context,
-    //     module,
-    //     builder: context.create_builder(),
-    //     execution_engine,
-    // };
-
-    // let sum = codegen
-    //     .jit_compile_sum()
-    //     .ok_or("Unable to JIT compile `sum`")?;
-
-    // let x = 1u64;
-    // let y = 2u64;
-    // let z = 3u64;
-
-    // unsafe {
-    //     println!("{} + {} + {} = {}", x, y, z, sum.call(x, y, z));
-    //     assert_eq!(sum.call(x, y, z), x + y + z);
-    // }
-
-    // Ok(())
 }
 
 #[cfg(test)]
