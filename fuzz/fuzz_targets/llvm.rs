51201ced82898b70c910f1b171781f3a99d80775
Martijn Faassen <faassen@startifact.com>
2022-08-27 18:05:43 +0200

    Use nicer API

--- fuzz/fuzz_targets/llvm.rs
+++ fuzz/fuzz_targets/llvm.rs
@@ -1,8 +1,8 @@
 #![no_main]
 extern crate aleven;
 use aleven::assemble::Assembler;
-use aleven::lang::Program;
 use aleven::llvm::CodeGen;
+use aleven::program::Program;
 use inkwell::context::Context;
 use inkwell::OptimizationLevel;
 use libfuzzer_sys::fuzz_target;
@@ -10,25 +10,14 @@ use libfuzzer_sys::fuzz_target;
 fuzz_target!(|data: &[u8]| {
     let assembler = Assembler::new();
     let instructions = assembler.disassemble(data);
-    let instructions = Program::cleanup(&instructions);
+
+    let program = Program::new(&instructions);
 
     let context = Context::create();
-    let module = context.create_module("program");
-    let execution_engine = module
-        .create_jit_execution_engine(OptimizationLevel::None)
-        .expect("Execution engine couldn't be built");
-    let codegen = CodeGen {
-        context: &context,
-        module,
-        builder: context.create_builder(),
-        execution_engine,
-    };
-    let program = codegen
-        .compile_program(&instructions, data.len() as u16)
-        .expect("Unable to JIT compile `program`");
+    let codegen = CodeGen::new(&context);
+
+    let func = program.compile(&codegen, data.len() as u16);
     codegen.module.verify().unwrap();
     let mut memory = data.to_vec();
-    unsafe {
-        program.call(memory.as_mut_ptr());
-    }
+    Program::run(func, &mut memory);
 });
