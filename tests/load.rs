85075b1f371119eefb3d8c51326f4de21028b422
Martijn Faassen <faassen@startifact.com>
2022-08-29 15:06:06 +0200

    Factor out store tests, more load tests

--- tests/load.rs
+++ tests/load.rs
@@ -1,4 +1,5 @@
-use aleven::{Instruction, Load, Store};
+use aleven::{Immediate, Instruction, Load, Store};
+use byteorder::{ByteOrder, LittleEndian};
 use parameterized::parameterized;
 
 mod run;
@@ -126,4 +127,100 @@ fn test_lh_out_of_bounds(runner: Runner) {
     let mut memory = [0u8; 64];
     runner(&instructions, &mut memory);
     assert_eq!(memory, [0u8; 64]);
+}
+
+#[parameterized(runner={run_llvm, run_interpreter})]
+fn test_lb_sign_extends(runner: Runner) {
+    let instructions = [
+        Instruction::Lb(Load {
+            offset: 0,
+            rs: 1,
+            rd: 2,
+        }),
+        Instruction::Sh(Store {
+            offset: 10,
+            rs: 2,
+            rd: 3, // defaults to 0
+        }),
+    ];
+    let mut memory = [0u8; 64];
+    memory[0] = -4i8 as u8; // FFFC
+    runner(&instructions, &mut memory);
+    let value = LittleEndian::read_i16(&memory[20..]);
+    assert_eq!(value, -4);
+}
+
+#[parameterized(runner={run_llvm, run_interpreter})]
+fn test_lbu_zero_extends(runner: Runner) {
+    let instructions = [
+        Instruction::Lbu(Load {
+            offset: 0,
+            rs: 1,
+            rd: 2,
+        }),
+        Instruction::Sh(Store {
+            offset: 10,
+            rs: 2,
+            rd: 3, // defaults to 0
+        }),
+    ];
+    let mut memory = [0u8; 64];
+    memory[0] = -4i8 as u8; // FC
+    runner(&instructions, &mut memory);
+    let value = LittleEndian::read_i16(&memory[20..]);
+    assert_eq!(value, 252);
+}
+
+#[parameterized(runner={run_llvm, run_interpreter})]
+fn test_lb_sign_extends_with_sra(runner: Runner) {
+    let instructions = [
+        Instruction::Lb(Load {
+            offset: 0,
+            rs: 1,
+            rd: 2,
+        }),
+        Instruction::Srai(Immediate {
+            value: 2,
+            rs: 2,
+            rd: 2,
+        }),
+        Instruction::Sh(Store {
+            offset: 10,
+            rs: 2,
+            rd: 3, // defaults to 0
+        }),
+    ];
+    let mut memory = [0u8; 64];
+    memory[0] = -4i8 as u8; // FFFC
+    runner(&instructions, &mut memory);
+    let value = LittleEndian::read_i16(&memory[20..]);
+    assert_eq!(value, -1);
+    assert_eq!(value, 0xFFFFu16 as i16);
+}
+
+#[parameterized(runner={run_llvm, run_interpreter})]
+fn test_lbu_zero_extends_sra(runner: Runner) {
+    let instructions = [
+        Instruction::Lbu(Load {
+            offset: 0,
+            rs: 1,
+            rd: 2,
+        }),
+        Instruction::Srai(Immediate {
+            value: 2,
+            rs: 2,
+            rd: 2,
+        }),
+        Instruction::Sh(Store {
+            offset: 10,
+            rs: 2,
+            rd: 3, // defaults to 0
+        }),
+    ];
+    let mut memory = [0u8; 64];
+    memory[0] = -4i8 as u8;
+    // lbu interprets this as 252, or 0000011111100
+    runner(&instructions, &mut memory);
+    let value = LittleEndian::read_u16(&memory[20..]);
+    assert_eq!(value, 63);
 }
