01951047bbd0c2d97db62013c11f43cc99768a7c
Martijn Faassen <faassen@startifact.com>
2022-09-01 17:20:00 +0200

    Let's call this the assembler now

--- src/lib.rs
+++ src/lib.rs
@@ -2,21 +2,21 @@ extern crate num;
 #[macro_use]
 extern crate num_derive;
 
+mod assembler;
 mod cache;
 mod function;
 mod lang;
 mod llvm;
 mod llvmasm;
 mod opcodetype;
-mod parser;
 mod program;
 pub mod run;
 mod serializer;
 
+pub use assembler::parse;
 pub use cache::FunctionValueCache;
 pub use function::Function;
 pub use lang::{Branch, BranchTarget, CallId, Immediate, Instruction, Load, Register, Store};
 pub use llvm::CodeGen;
-pub use parser::parse;
 pub use program::Program;
 pub use serializer::Serializer;
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ extern crate num;
 #[macro_use]
 extern crate num_derive;
 
+pub mod assembler;
 pub mod cache;
 pub mod function;
 pub mod lang;
 pub mod llvm;
 pub mod llvmasm;
 pub mod opcodetype;
-pub mod parser;
 pub mod program;
 pub mod run;
 pub mod serializer;
