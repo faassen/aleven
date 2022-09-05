13049d4ac0755547a8ccbb9888244f5295d8e7e5
Martijn Faassen <faassen@startifact.com>
2022-09-05 19:46:11 +0200

    reimplement the run_*_func in terms of running a program with one func

--- src/run.rs
+++ src/run.rs
@@ -8,20 +8,6 @@ use inkwell::context::Context;
 pub type RunnerFunc = fn(&[Instruction], &mut [u8]);
 pub type RunnerProgram = fn(&[&[Instruction]], &mut [u8]);
 
-pub fn run_llvm_func(instructions: &[Instruction], memory: &mut [u8]) {
-    let program = Program::from_instructions(instructions);
-    let context = Context::create();
-    let codegen = CodeGen::new(&context);
-    let mut cache = FunctionValueCache::new();
-    let func = program.compile(0, &codegen, memory.len() as u16, &mut cache);
-    codegen.module.verify().unwrap();
-    Function::run(&func, memory);
-}
-
-pub fn run_interpreter_func(instructions: &[Instruction], memory: &mut [u8]) {
-    Program::from_instructions(instructions).interpret(memory);
-}
-
 pub fn run_interpreter_program(funcs: &[&[Instruction]], memory: &mut [u8]) {
     let program = Program::new(funcs);
     program.interpret(memory);
@@ -35,4 +21,12 @@ pub fn run_llvm_program(funcs: &[&[Instruction]], memory: &mut [u8]) {
     let func = program.compile(0, &codegen, memory.len() as u16, &mut cache);
     codegen.module.verify().unwrap();
     Function::run(&func, memory);
+}
+
+pub fn run_llvm_func(instructions: &[Instruction], memory: &mut [u8]) {
+    run_llvm_program(&[instructions], memory);
+}
+
+pub fn run_interpreter_func(instructions: &[Instruction], memory: &mut [u8]) {
+    run_interpreter_program(&[instructions], memory);
 }
