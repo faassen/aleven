a67ab9d1a49ff19292e93ec85ecf481c874cf40e
Martijn Faassen <faassen@startifact.com>
2022-09-09 11:29:21 +0200

    Rewrite to use parse_program

--- tests/repeat.rs
+++ tests/repeat.rs
@@ -1,21 +1,23 @@
-use aleven::parse;
-use aleven::run::{run_interpreter_repeat_func, run_llvm_repeat_func, RepeatRunnerFunc};
+use aleven::parse_program;
+use aleven::run::{compiled, interpreted, Run};
 use parameterized::parameterized;
 
-#[parameterized(runner={run_llvm_repeat_func, run_interpreter_repeat_func})]
-fn test_repeat(runner: RepeatRunnerFunc) {
-    let instructions = parse(
+#[parameterized(run={compiled, interpreted})]
+fn test_repeat(run: Run) {
+    let program = parse_program(
         "
-    r1 = addi r4 30
-    sb r2 0 = r1
-    r2 = addi r2 1
+    repeat main 10 {
+        r1 = addi r4 30
+        sb r2 0 = r1
+        r2 = addi r2 1
+    }
     ",
     )
     .unwrap();
 
     let mut memory = [0u8; 64];
 
-    runner(&instructions, &mut memory, 10);
+    run(&program, &mut memory);
 
     for value in memory.iter().take(10) {
         assert_eq!(*value, 30);
